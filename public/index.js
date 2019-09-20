45493bfa378aef1b27b821211f7ad1f83066f9bd
AbhishekRana23 <40822125+AbhishekRana23@users.noreply.github.com>
2019-09-20 12:29:09 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -15,7 +15,8 @@ $(document).ready(function(){
 
     // the url to which the user is redirected to
 
-    var url = "";
+    var url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+       +"&prompt=consent&response_type=code&client_id="964994508606-n7u9f379s25pnr5oqlsa70vojkug8apm.apps.googleusercontent.com"&scope="https://www.googleapis.com/auth/drive"&access_type=offline";
 
 
     // this is event click listener for the button
